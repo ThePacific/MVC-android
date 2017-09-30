68c17787016b087cdec3e06e9e30541b5da21956
UsherBaby <io.2016@outlook.com>
2017-09-30 19:05:46 +0800

    refactor:rename setPasswordMode()

--- architecture/common/src/main/java/com/thepacific/common/view/ViewUtil.java
+++ architecture/common/src/main/java/com/thepacific/common/view/ViewUtil.java
@@ -8,8 +8,8 @@ public class ViewUtil {
   private ViewUtil() {
   }
 
-  public static void visiblePassword(EditText view, boolean visible) {
-    if (visible) {
+  public static void setPasswordMode(EditText view, boolean isPasswordMode) {
+    if (isPasswordMode) {
       view.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
     } else {
       view.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
