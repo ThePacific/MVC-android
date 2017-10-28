ce0955a4578950f2b0fc7b6c52005ac6647cb1ff
UsherBaby <io.2016@outlook.com>
2017-10-28 09:47:36 +0800

    simplify

--- architecture/presentation/src/main/java/com/thepacific/presentation/core/OkReceiver.java
+++ architecture/presentation/src/main/java/com/thepacific/presentation/core/OkReceiver.java
@@ -14,7 +14,7 @@ import javax.inject.Inject;
 
 public final class OkReceiver extends BroadcastReceiver {
 
-  static final String FINISH_ACTION = "com.boo.presentation.finish";
+  static final String FINISH_ACTION = "com.pacific.presentation.finish";
   private final ArrayMap<String, Consumer> consumers = new ArrayMap<>();
 
   @Inject
