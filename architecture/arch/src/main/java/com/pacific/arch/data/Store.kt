a24f013c7baefa674143d8c9e79a77f724cc0aab
UsherBaby <thepacific2016@aol.com>
2018-02-11 22:08:37 +0800

    Fix

--- architecture/arch/src/main/java/com/pacific/arch/data/Store.kt
+++ architecture/arch/src/main/java/com/pacific/arch/data/Store.kt
@@ -1,4 +1,21 @@
 package com.pacific.arch.data
 
-class Store {
+import io.reactivex.Completable
+import io.reactivex.Maybe
+import io.reactivex.Single
+
+interface Store<in K, V> {
+    fun get(key: K): Maybe<out V>
+    fun getAll(key: K): Maybe<List<V>>
+
+    fun fetch(key: K)
+    fun request(key: K): Single<V>
+
+    fun delete(value: V): Completable
+    fun deleteAll(values: Maybe<List<V>>): Completable
+
+    fun push(value: V): Completable
+    fun pushAll(values: Maybe<List<V>>): Completable
+
+    fun clear()
 }
