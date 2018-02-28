f602ad8a697f152f673d2967710ecac1704d10b6
Barrey.L <Barrey.L@iv66.net>
2018-02-28 18:03:09 +0800

    Update

--- architecture/example/src/main/java/com/pacific/example/feature/main/MainActivity.kt
+++ architecture/example/src/main/java/com/pacific/example/feature/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.design.widget.TabLayout
 import android.support.v4.view.GravityCompat
+import android.support.v4.view.MenuItemCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.view.Menu
 import android.view.MenuItem
@@ -13,14 +14,17 @@ import com.pacific.arch.presentation.activityViewModel
 import com.pacific.arch.presentation.contentView
 import com.pacific.arch.views.widget.OnTabSelected
 import com.pacific.example.base.BaseActivity
+import com.pacific.example.views.SyncActionProvider
 import javax.inject.Inject
 
 class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
     private val binding: ActivityMainBinding by contentView(R.layout.activity_main)
     val model by activityViewModel(MainViewModel::class.java)
-    
+
     @Inject
     lateinit var mainFragmentAdapter: MainFragmentAdapter
+
+    private lateinit var menu: Menu
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,20 +49,26 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         binding.viewPager.clearOnPageChangeListeners()
         binding.viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(binding.tabLayout))
         binding.viewPager.adapter = mainFragmentAdapter
-
-        onSearchRequested()
     }
 
     override fun onBackPressed() {
         if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
             binding.drawerLayout.closeDrawer(GravityCompat.START)
+            filmToolbarMenu()
         } else {
             super.onBackPressed()
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
+        this.menu = menu
+        when (mainFragmentAdapter.currentPosition) {
+            0 -> homeToolbarMenu()
+            1 -> gameToolbarMenu()
+            2 -> filmToolbarMenu()
+            3 -> bookToolbarMenu()
+            4 -> newsToolbarMenu()
+        }
         return true
     }
 
@@ -89,5 +99,28 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         }
         binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    fun homeToolbarMenu() {
+        menuInflater.inflate(R.menu.fragment_home_toolbar, menu)
+        val syncProvider = MenuItemCompat.getActionProvider(menu.findItem(R.id.action_sync)) as SyncActionProvider
+
+    }
+
+    fun newsToolbarMenu() {
+        menu.findItem(R.id.action_sync).isVisible = false
+
+    }
+
+    fun filmToolbarMenu() {
+        menu.findItem(R.id.action_sync).isVisible = false
+    }
+
+    fun gameToolbarMenu() {
+        menu.findItem(R.id.action_sync).isVisible = false
+    }
+
+    fun bookToolbarMenu() {
+        menu.findItem(R.id.action_sync).isVisible = false
     }
 }
